0b001a401529bea1546466b3ce9fd1c51d72ab1b
thibaultstadsbader <115148525+thibaultstadsbader@users.noreply.github.com>
2024-03-22 23:38:33 +0100

    Labo 6 Update

--- Labo 3/Opdracht colorpicker/scripts/code.js
+++ Labo 3/Opdracht colorpicker/scripts/code.js
@@ -1,4 +1,3 @@
-
 const setup = () => {
     let sliders = document.getElementsByClassName("slider");
     for (let i = 0; i < sliders.length; i++) {
@@ -9,12 +8,12 @@ const setup = () => {
 }
 
 const update = () => {
-    let colorbox = document.getElementById("blokje");
-    let sliders = document.getElementsByClassName("slider");
-    let labels = document.getElementsByClassName("label");
+    let colorbox = document.querySelector(".colorBoxRond")
+    let sliders = document.querySelectorAll(".slider");
+    let labels = document.querySelectorAll(".label");
 
     for (let i = 0; i < sliders.length; i++) {
-        labels[i].innerHTML = (sliders[i].value * 2.55).toFixed(0);
+        labels[i].textContent = (sliders[i].value * 2.55).toFixed(0);
     }
 
     colorbox.style.backgroundColor = "rgb(" + (sliders[0].value * 2.55) + ", " + (sliders[1].value * 2.55) + ", " + (sliders[2].value * 2.55) + ")";
